4e250fcd66f39602a851ba7f5c18e6da7d2eff4f
Kirill Tarasov <tarasovki94@gmail.com>
2020-03-06 03:03:05 +0500

    Updates

---
+++ precache-manifest.c529dca4d064d4210f902711b43b48dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2fea4fa4d38e9c14fe3b82cde013ca",
+    "url": "/github-search/index.html"
+  },
+  {
+    "revision": "65dc47b4a740f5c8f02f",
+    "url": "/github-search/static/css/main.f95702ab.chunk.css"
+  },
+  {
+    "revision": "a6db18a68098d0567e4a",
+    "url": "/github-search/static/js/2.f9a9bdff.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/github-search/static/js/2.f9a9bdff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65dc47b4a740f5c8f02f",
+    "url": "/github-search/static/js/main.e2156764.chunk.js"
+  },
+  {
+    "revision": "432226701b6ef4d1067c",
+    "url": "/github-search/static/js/runtime-main.51caba1d.js"
+  }
+]);
